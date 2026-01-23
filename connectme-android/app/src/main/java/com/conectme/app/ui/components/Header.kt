32420aaa85b848e8d6fe695c3601620ed3b49d52
juan vivenes <vivenesjuan332@gmail.com>
2026-01-23 17:42:58 +0000

    fix(android): Add MaterialTheme import to Header.kt

--- connectme-android/app/src/main/java/com/conectme/app/ui/components/Header.kt
+++ connectme-android/app/src/main/java/com/conectme/app/ui/components/Header.kt
@@ -3,6 +3,7 @@ package com.conectme.app.ui.components
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
