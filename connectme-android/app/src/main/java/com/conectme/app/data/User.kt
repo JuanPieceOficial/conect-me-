69365763413688e5b372199b660c61355b08a9d4
juan vivenes <vivenesjuan332@gmail.com>
2026-01-23 19:34:37 +0000

    debug(android): Temporarily make User fields non-nullable

--- connectme-android/app/src/main/java/com/conectme/app/data/User.kt
+++ connectme-android/app/src/main/java/com/conectme/app/data/User.kt
@@ -5,7 +5,10 @@ data class User(
     val username: String,
     val name: String,
     val avatar: String,
+    val coverPhoto: String,
     val bio: String,
     val followers: Int,
-    val following: Int
+    val following: Int,
+    val isFriend: Boolean,
+    val isFollowing: Boolean
 )
